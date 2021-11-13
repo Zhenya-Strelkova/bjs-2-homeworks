4df2bf34f26719f84e9f01ed51ee756064e30bb7
Zhenya-Strelkova <zhenya.strelkova@mail.ru>
2021-11-13 23:03:25 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,35 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let D = b ** 2 - 4 * a * c;
+  if (D > 0) {
+    arr[0] = (-b + Math.sqrt(D)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(D)) / (2 * a);
+  } else if (D === 0) {
+    arr[0] = (-b) / (2 * a);
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  //валидация данных
+  if (isNaN(+percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение \"${percent}\"`;
+  }
+  if (isNaN(+contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение \"${contribution}\"`;
+  }
+  if (isNaN(+amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение \"${amount}\"`;
+  }
 
-  // код для задачи №2 писать здесь
-
+  //расчет выплат
+  const currentDate = new Date();
+  const colMonth = (date.getFullYear() - currentDate.getFullYear()) * 12 + (date.getMonth() - currentDate.getMonth()) +
+      Number(date.getDate() > currentDate.getDate());
+  const p = (1 / 12) * (percent / 100);
+  totalAmount = +(colMonth * (amount - contribution) * p * (1 + 1 / ((1 + p) ** colMonth - 1))).toFixed(2);
   return totalAmount;
 }
