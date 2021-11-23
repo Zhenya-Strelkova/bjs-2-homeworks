9602aa33259f7564da30d78278e38e9a5d597327
Zhenya-Strelkova <zhenya.strelkova@mail.ru>
2021-11-23 12:54:18 +0300

    solve task #3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  if (arr1.length !== arr2.length) {
+    result = false;
+  } else {
+    result = arr1.every((item, idx) => item === arr2[idx]);
+  }
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr
+      .filter((item) => item > 0 && item % 3 === 0)
+      .map((item) => item * 10);
 
   return resultArr; // array
 }
