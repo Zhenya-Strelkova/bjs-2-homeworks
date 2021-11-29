988e53748bf1327e66251c324ea212a5d8d9ff63
Zhenya-Strelkova <zhenya.strelkova@mail.ru>
2021-11-30 01:21:12 +0300

    solve task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (!this.hasOwnProperty('marks')) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    if (!this.hasOwnProperty('marks')) {
+        this.marks = [...marks];
+    } else {
+        this.marks.push(...marks);
+    }
+}
+
+Student.prototype.getAverage = function () {
+    if (!this.hasOwnProperty('marks')) {
+        return 0;
+    } else {
+        return this.marks.reduce((a, b) => a + b) / this.marks.length;
+    }
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
