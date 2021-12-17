4c8550a959333a7c5392b1804bd2fe9be07725a5
Zhenya-Strelkova <zhenya.strelkova@mail.ru>
2021-12-17 15:37:17 +0300

    solve task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,51 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+      const key = args.join(',');
+      const itemCache = cache.find((item) => item.hasOwnProperty(key));
+      if (itemCache !== undefined) {
+          console.log(`Из кэша: ${itemCache[key]}`);
+          return `Из кэша: ${itemCache[key]}`;
+      } else {
+          let result = func(...args);
+          if (cache.length === 5) cache.shift();
+          cache.push({[key]: result,});
+          console.log(`Вычисляем: ${result}`);
+          return `Вычисляем: ${result}`;
+      }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let inTimer = false;
+  let timer = null;
+
+  function wrapper(...args) {
+      if (inTimer) {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+              inTimer = false;
+              func(...args);
+          }, ms);
+      } else {
+          inTimer = true;
+          timer = setTimeout(() => {
+              inTimer = false;
+              func(...args);
+          }, ms);
+          return func(...args);
+      }
+  }
+  return wrapper;
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  wrapper.count = 0;
+  function wrapper(...args) {
+      wrapper.count += 1;
+      return func(...args);
+  }
+  return wrapper;
 }
